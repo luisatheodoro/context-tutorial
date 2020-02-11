aba836b6053fec65f938cbb291cc720b0378d21a
Luisa P <luisa.paoliello@avadolearning.com>
2020-02-11 16:57:33 +0000

    context-tutorial

---
+++ src/components/BookList.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends Component {
+
+    render() { 
+        return (
+            <ThemeContext.Consumer>
+                {(context) => {
+                    const { isLightTheme, light, dark } = context;
+                    const theme = isLightTheme ? light : dark;
+                    return (
+                        <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+                            <ul>
+                                <li style={{ background: theme.ui }}>The way of kings</li>
+                                <li style={{ background: theme.ui }}>The name of the wind</li>
+                                <li style={{ background: theme.ui }}>The final empire</li>
+                            </ul>
+                        </div>
+                    )
+                }}
+            </ThemeContext.Consumer>
+          );
+    }
+}
+ 
+export default BookList;
